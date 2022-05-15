704b03b085a529029cfcb6b8c1c88398f86151f0
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-15 22:26:10 +0200

    "ricing"

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -18,9 +18,9 @@ static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "Px437 IBM BIOS:size=7:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray2[]       = "#000000";
+static const char col_gray3[]       = "#eeeeee";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -193,7 +193,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return,	spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,	spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_a,      togglebar,      {0} },
-	{ MODKEY,              XK_z,      togglegaps,     {0} },
+	{ MODKEY,              XK_a,      togglegaps,     {0} },
 	// { MODKEY|ShiftMask,    XK_a,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_j,				focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,				focusstack,     {.i = -1 } },
