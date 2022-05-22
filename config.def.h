88a5b42290c44fc97a93069924f9b5a08d3d1a9f
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-23 00:56:05 +0200

    ""

--- config.def.h
+++ config.def.h
@@ -249,7 +249,8 @@ static Key keys[] = {
 	TAGKEYS(                  	      XK_7,												6)
 	TAGKEYS(                  	      XK_8,												7)
 	TAGKEYS(                  	      XK_9,												8)
-	{ MODKEY|ShiftMask,       	      XK_q,				quit,           {0} },
+	// { MODKEY|ShiftMask,       	      XK_q,				quit,           {0} },
+	{ MODKEY|ShiftMask,								XK_q,				spawn,					SHCMD("killall startdwm") },
 };
 
 /* button definitions */
