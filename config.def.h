7a504ef9e6aeb37a02e0bca630f2adeea510f643
Christian Augustin <christianaugustinmusic@gmail.com>
2022-05-24 03:38:09 +0200

    "fixedsys"

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "Px437 IBM BIOS:size=7:antialias=true:autohint=true"};
+static const char *fonts[]          = { "Fixedsys Excelsior:size=12:antialias=false:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
