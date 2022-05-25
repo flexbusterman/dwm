65c9eb89c5a2cd2a7a8075dc87fe5a2f1b1f001b
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-25 13:13:32 +0200

    "calc shortcut"

--- config.def.h
+++ config.def.h
@@ -106,6 +106,7 @@ static Key keys[] = {
 	// bindings to run scripts
 	
 	{ MODKEY|ShiftMask,								XK_r,					spawn,          SHCMD("st -e dwmrebuild" ) },
+	{ MODKEY|ShiftMask,								XK_c,					spawn,          SHCMD("calc" ) },
 	{ MODKEY|ShiftMask|ControlMask,		XK_l,					spawn,					SHCMD("slock" ) },
 	{ MODKEY,                       	XK_d,					spawn,					SHCMD("sleep 0.2 && xdotool type --clearmodifiers \"$(date +\"%F \")\"")},
 	{ MODKEY,                       	XK_b,					spawn,					SHCMD("bluetoothconnect 74:5C:4B:D2:86:F7")},
