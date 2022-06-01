467696ba9de42de93fb5365d53fffa6d5edb0cf3
Christian Augustin <augustinsupremacy@gmail.com>
2022-06-01 15:30:20 +0200

    "window rules"

--- config.def.h
+++ config.def.h
@@ -40,8 +40,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,				NULL,       "Server",		1 << 8,       0,           -1 },
+	{ NULL,				NULL,       "Notes",		1 << 1,       0,           -1 },
+	{ NULL,				NULL,       "Git",			1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
