3ae33cdcf8343bbb0953c3f064232a453ebb20c7
Christian Augustin <christianaugustinmusic@gmail.com>
2022-12-14 21:49:46 +0100

    "original font"

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "MxPlus IBM BIOS:size=8:antialias=true:autohint=true"};
+static const char *fonts[]          = { "PxPlus IBM VGA 9x16:size=12:antialias=false:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
