4793a9475a838a8bb5d5c1ea89cfc6fbc7873f2d
Christian Augustin <christianaugustinmusic@gmail.com>
2023-05-03 13:45:41 +0200

    "firefox"

--- config.def.h
+++ config.def.h
@@ -205,7 +205,7 @@ static Key keys[] = {
 
 	{ MODKEY,													XK_q,					spawn,          SHCMD("qpwgraph" ) },
 	{ MODKEY|ShiftMask,             	XK_period,	  spawn,					SHCMD("qutebrowser -R" ) },
-	{ MODKEY|ShiftMask|ControlMask,  	XK_period,	  spawn,					SHCMD("bravestart" ) },
+	{ MODKEY|ShiftMask|ControlMask,  	XK_period,	  spawn,					SHCMD("firefoxstart" ) },
 	{ MODKEY|ShiftMask,								XK_x,					spawn,					SHCMD("xkill") },
 	{ MODKEY|ShiftMask,								XK_F4,				spawn,					SHCMD("pavucontrol") },
 	{ ShiftMask|ControlMask,				 	XK_Return,		spawn,					SHCMD("audacious")},
