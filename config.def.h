eafa23601f3eccea715349f5b72303eae5fc3812
Christian Augustin <christianaugustinmusic@gmail.com>
2024-02-29 03:42:57 +0100

    "font"

--- config.def.h
+++ config.def.h
@@ -15,7 +15,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Mono Medium:size=11:antialias=true"};
+// static const char *fonts[]          = { "Fira Mono Medium:size=11:antialias=true"};
+static const char *fonts[]          = { "PxPlus IBM VGA 8x16:size=11.25:style=Regular:antialias=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
