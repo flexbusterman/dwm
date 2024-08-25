a8bc32e92ec89dead03bbea10d3d4abc8c1186b9
Christian Augustin <christianaugustinmusic@gmail.com>
2024-08-25 20:28:03 +0200

    no borders and start with bar hidden

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
@@ -13,10 +13,10 @@ static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corne
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
+static const int showbar            = 0;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-// static const char *fonts[]          = { "Fira Mono Medium:size=11:antialias=true"};
-static const char *fonts[]          = { "PxPlus IBM VGA 8x16:size=11.25:style=Regular:antialias=true"};
+static const char *fonts[]          = { "Fira Mono Medium:size=11:antialias=true"};
+// static const char *fonts[]          = { "PxPlus IBM VGA 8x16:size=11.25:style=Regular:antialias=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
@@ -96,7 +96,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "$TERM", NULL };
 static const char scratchpadname[] = "scratchpad";
 // static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x40", NULL };
 /* static const char *scratchpadcmd[] = {"st", "-t", scratchpadname, "-g", "80x45", "-e", "nvim", "/home/flex/Dropbox/NOTES/TODO.md", NULL}; */
@@ -251,9 +251,9 @@ static Key keys[] = {
 	{ WINKEY|ControlMask|ShiftMask,		XK_j,					tagmon,					{.i = -1 } },
 	{ MODKEY|ShiftMask,								XK_j,					movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,								XK_k,					movestack,      {.i = -1 } },
-	{ MODKEY,               	        XK_Return,		spawn,          {.v = dmenucmd } },
+	// { MODKEY,               	        XK_Return,		spawn,          {.v = dmenucmd } },
 	/* { MODKEY,						     	        XK_period,		spawn,   	      SHCMD("tmuxmain") }, */
-	{ MODKEY|ShiftMask,		     	      XK_Return,		spawn,   	      SHCMD("st") },
+	// { MODKEY|ShiftMask,		     	      XK_Return,		spawn,   	      SHCMD("st") },
 	{ MODKEY,							  	        XK_grave,		  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY, 							XK_z,    		  togglebar,      {0} },
 	{ MODKEY,         			XK_a,    		  togglegaps,     {0} },
