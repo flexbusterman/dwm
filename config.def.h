db119c1ecb7baec7eceda14538f3210dba3baee2
Christian Augustin <johnchristianaugustin@gmail.com>
2025-12-06 11:12:40 +0100

    bindings

--- config.def.h
+++ config.def.h
@@ -64,8 +64,8 @@ static const int refreshrate =
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
-    {"><>", NULL}, /* no layout function means floating behavior */
     {"[M]", monocle},
+    {"><>", NULL}, /* no layout function means floating behavior */
 };
 
 /* key definitions */
