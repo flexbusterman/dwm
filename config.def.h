5b5a704b7cf74071be99aa535f830dddcc9a5343
Christian Augustin <johnchristianaugustin@gmail.com>
2026-03-21 00:39:17 +0100

    blue

--- config.def.h
+++ config.def.h
@@ -28,14 +28,15 @@ static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+// static const char col_cyan[] = "#005577";
+static const char col_blue[] = "#000087";
 static const char col_red[] = "#ff0000";
 static const char col_yellow[] = "#ffff00";
 static const char col_white[] = "#ffffff";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_black},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+    [SchemeSel] = {col_gray4, col_blue, col_blue},
     [SchemeWarn] = {col_black, col_yellow, col_red},
     // [SchemeUrgent] = {col_white, col_red, col_red},
     [SchemeUrgent] = {col_gray3, col_gray1, col_black},
@@ -93,7 +94,7 @@ static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "-nf",       col_gray3, "-sb",    col_blue, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"kitty -e tmux new-session -A -s main", NULL};
 
 #include "movestack.c"
